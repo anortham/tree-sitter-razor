2b1fbff4889d4647897325ad5a222de45e675c54
Alan Northam <anortham@gmail.com>
2026-07-13 12:32:06 -0500

    Repair Swift binding

--- bindings/swift/TreeSitterRazorTests/TreeSitterRazorTests.swift
+++ bindings/swift/TreeSitterRazorTests/TreeSitterRazorTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import XCTest
 import SwiftTreeSitter
 import TreeSitterRazor
@@ -8,5 +9,40 @@ final class TreeSitterRazorTests: XCTestCase {
         let language = Language(language: tree_sitter_razor())
         XCTAssertNoThrow(try parser.setLanguage(language),
                          "Error loading Razor grammar")
+    }
+
+    func testParsesMixedRazorDocument() throws {
+        let parser = Parser()
+        let language = Language(language: tree_sitter_razor())
+        try parser.setLanguage(language)
+
+        let source = """
+        @page
+        <EditForm Model="@model"><InputDate @bind-Value="model.Date" /></EditForm>
+        """
+        let tree = try XCTUnwrap(parser.parse(source))
+        let root = try XCTUnwrap(tree.rootNode)
+
+        XCTAssertEqual(root.nodeType, "compilation_unit")
+        XCTAssertFalse(root.hasError)
+    }
+
+    func testLoadsPackagedHighlightQuery() throws {
+        let language = Language(language: tree_sitter_razor())
+        let testBundle = Bundle(for: Self.self)
+        let resourceBundleURL = testBundle.bundleURL
+            .deletingLastPathComponent()
+            .appendingPathComponent("TreeSitterRazor_TreeSitterRazor.bundle")
+        let resourceBundle = try XCTUnwrap(Bundle(url: resourceBundleURL))
+        let queriesURL = try XCTUnwrap(
+            resourceBundle.url(forResource: "queries", withExtension: nil)
+        )
+        let configuration = try LanguageConfiguration(
+            language,
+            name: "Razor",
+            queriesURL: queriesURL
+        )
+
+        XCTAssertNotNil(configuration.queries[.highlights])
     }
 }
