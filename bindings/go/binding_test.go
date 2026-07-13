1dae8611d51d4bb161aa9f310999af6c3b4c545a
Alan Northam <anortham@gmail.com>
2026-07-13 12:30:53 -0500

    Repair Go binding

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -7,9 +7,21 @@ import (
 	tree_sitter_razor "github.com/tree-sitter/tree-sitter-razor/bindings/go"
 )
 
-func TestCanLoadGrammar(t *testing.T) {
+func TestCanParseMixedRazor(t *testing.T) {
 	language := tree_sitter.NewLanguage(tree_sitter_razor.Language())
-	if language == nil {
-		t.Errorf("Error loading Razor grammar")
+	parser := tree_sitter.NewParser()
+	t.Cleanup(parser.Close)
+	if err := parser.SetLanguage(language); err != nil {
+		t.Fatalf("set Razor language: %v", err)
+	}
+
+	tree := parser.Parse([]byte("@page\n<EditForm Model=\"@model\"><InputDate @bind-Value=\"model.Date\" /></EditForm>"), nil)
+	t.Cleanup(tree.Close)
+	root := tree.RootNode()
+	if root.Kind() != "compilation_unit" {
+		t.Fatalf("root kind = %q, want %q", root.Kind(), "compilation_unit")
+	}
+	if root.HasError() {
+		t.Fatalf("parse contains errors: %s", root.ToSexp())
 	}
 }
